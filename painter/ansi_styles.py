798b9d0c4104b07bcbd9e34afa0166f86006c0e7
Fotis Gimian <fgimiansoftware@gmail.com>
2014-04-26 13:28:00 +1000

    Removed the use of OrderedDict which wasn't required

--- painter/ansi_styles.py
+++ painter/ansi_styles.py
@@ -1,35 +1,33 @@
-from collections import OrderedDict
+ANSI_CODES = {
+    'reset': (0, 0),
 
-ANSI_CODES = OrderedDict([
-    ('reset', (0, 0)),
+    'bold': (1, 22),
+    'italic': (3, 23),
+    'underline': (4, 24),
+    'blink': (5, 25),
+    'inverse': (7, 27),
+    'strikethrough': (9, 29),
 
-    ('bold', (1, 22)),
-    ('italic', (3, 23)),
-    ('underline', (4, 24)),
-    ('blink', (5, 25)),
-    ('inverse', (7, 27)),
-    ('strikethrough', (9, 29)),
+    'black': (30, 39),
+    'red': (31, 39),
+    'green': (32, 39),
+    'yellow': (33, 39),
+    'blue': (34, 39),
+    'magenta': (35, 39),
+    'cyan': (36, 39),
+    'white': (37, 39),
+    'gray': (90, 39),
+    'grey': (90, 39),
 
-    ('black', (30, 39)),
-    ('red', (31, 39)),
-    ('green', (32, 39)),
-    ('yellow', (33, 39)),
-    ('blue', (34, 39)),
-    ('magenta', (35, 39)),
-    ('cyan', (36, 39)),
-    ('white', (37, 39)),
-    ('gray', (90, 39)),
-    ('grey', (90, 39)),
-
-    ('on_black', (40, 49)),
-    ('on_red', (41, 49)),
-    ('on_green', (42, 49)),
-    ('on_yellow', (43, 49)),
-    ('on_blue', (44, 49)),
-    ('on_magenta', (45, 49)),
-    ('on_cyan', (46, 49)),
-    ('on_white', (47, 49))
-])
+    'on_black': (40, 49),
+    'on_red': (41, 49),
+    'on_green': (42, 49),
+    'on_yellow': (43, 49),
+    'on_blue': (44, 49),
+    'on_magenta': (45, 49),
+    'on_cyan': (46, 49),
+    'on_white': (47, 49)
+}
 
 
 class AnsiStyle(object):
@@ -60,7 +58,7 @@ class AnsiStyle(object):
 
 class AnsiStyler(object):
     def __init__(self):
-        self.styles = OrderedDict()
+        self.styles = {}
         for key, (open_code, close_code) in ANSI_CODES.items():
             self.styles[key] = AnsiStyle(open_code, close_code)
 
