e8b313c7f56ddc5045b5081b7919fbb22063ff4e
Fotis Gimian <fgimiansoftware@gmail.com>
2014-04-26 10:44:53 +1000

    Converted ansi codes into an ordered dict and made it a constant

--- painter/ansi_styles.py
+++ painter/ansi_styles.py
@@ -1,33 +1,35 @@
-codes = {
-    'reset': (0, 0),
+from collections import OrderedDict
 
-    'bold': (1, 22),
-    'italic': (3, 23),
-    'underline': (4, 24),
-    'blink': (5, 25),
-    'inverse': (7, 27),
-    'strikethrough': (9, 29),
+ANSI_CODES = OrderedDict([
+    ('reset', (0, 0)),
 
-    'black': (30, 39),
-    'red': (31, 39),
-    'green': (32, 39),
-    'yellow': (33, 39),
-    'blue': (34, 39),
-    'magenta': (35, 39),
-    'cyan': (36, 39),
-    'white': (37, 39),
-    'gray': (90, 39),
+    ('bold', (1, 22)),
+    ('italic', (3, 23)),
+    ('underline', (4, 24)),
+    ('blink', (5, 25)),
+    ('inverse', (7, 27)),
+    ('strikethrough', (9, 29)),
 
-    'on_black': (40, 49),
-    'on_red': (41, 49),
-    'on_green': (42, 49),
-    'on_yellow': (43, 49),
-    'on_blue': (44, 49),
-    'on_magenta': (45, 49),
-    'on_cyan': (46, 49),
-    'on_white': (47, 49)
-}
-codes['grey'] = codes['gray']
+    ('black', (30, 39)),
+    ('red', (31, 39)),
+    ('green', (32, 39)),
+    ('yellow', (33, 39)),
+    ('blue', (34, 39)),
+    ('magenta', (35, 39)),
+    ('cyan', (36, 39)),
+    ('white', (37, 39)),
+    ('gray', (90, 39)),
+    ('grey', (90, 39)),
+
+    ('on_black', (40, 49)),
+    ('on_red', (41, 49)),
+    ('on_green', (42, 49)),
+    ('on_yellow', (43, 49)),
+    ('on_blue', (44, 49)),
+    ('on_magenta', (45, 49)),
+    ('on_cyan', (46, 49)),
+    ('on_white', (47, 49))
+])
 
 
 class Style(object):
@@ -49,8 +51,8 @@ class Style(object):
 
 class Styler(object):
     def __init__(self):
-        self.styles = {}
-        for key, (open_code, close_code) in codes.items():
+        self.styles = OrderedDict()
+        for key, (open_code, close_code) in ANSI_CODES.items():
             self.styles[key] = Style(open_code, close_code)
 
     def __contains__(self, key):
