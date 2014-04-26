a47f84ed3220310102d90dccdf70448301481dd9
Fotis Gimian <fgimiansoftware@gmail.com>
2014-04-26 10:44:08 +1000

    Added remaining imports to the painter package

--- painter/__init__.py
+++ painter/__init__.py
@@ -1,3 +1,6 @@
 __version__ = '0.2-dev'
 
+from .ansi_styles import ansi  # noqa
+from .has_color import has_color  # noqa
 from .painter import paint  # noqa
+from .strip_ansi import strip_ansi  # noqa
