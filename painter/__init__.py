e3e8f9e42e7cdc49866043a2bd630c6498b0969c
Fotis Gimian <fgimiansoftware@gmail.com>
2014-04-27 16:23:57 +1000

    Added all definition for painter package

--- painter/__init__.py
+++ painter/__init__.py
@@ -9,3 +9,10 @@ from .ansi_styles import ansi  # noqa
 from .has_color import has_color  # noqa
 from .painter import paint  # noqa
 from .strip_ansi import strip_ansi  # noqa
+
+__all__ = [
+    'ansi',
+    'has_color',
+    'paint',
+    'strip_ansi'
+]
