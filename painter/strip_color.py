c727085fa8e8736acef383737f4126265268e36c
Fotis Gimian <fgimiansoftware@gmail.com>
2014-04-28 12:59:15 +1000

    Updated strip_color function to work correctly on Python 2.6 again

--- painter/strip_color.py
+++ painter/strip_color.py
@@ -3,7 +3,7 @@ import re
 
 def strip_color(s):
     if isinstance(s, str):
-        return re.sub(r"""
+        return re.compile(r"""
             \x1b            # escape character
             \[              # the CSI code for color
             (               # start of color definition
@@ -11,6 +11,6 @@ def strip_color(s):
             (;[0-9]{1,3})*  # optional ANSI color properties (mainly xterm)
             )?              # end of color definition
             m               # the CSI SGR (Select Graphic Rendition) command
-        """, '', s, flags=re.VERBOSE)
+        """, re.VERBOSE).sub('', s)
     else:
         return s
