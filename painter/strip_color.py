e91488d85419666fb238ad317de2101b518df3e2
Fotis Gimian <fgimiansoftware@gmail.com>
2014-04-28 11:43:56 +1000

    Repaired a bug whereby strip_color would not remove colors from ls output (fixes #1)

--- painter/strip_color.py
+++ painter/strip_color.py
@@ -3,8 +3,14 @@ import re
 
 def strip_color(s):
     if isinstance(s, str):
-        return re.sub(
-            r'\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?[m|K]', '', s
-        )
+        return re.sub(r"""
+            \x1b            # escape character
+            \[              # the CSI code for color
+            (               # start of color definition
+            [0-9]{1,3}      # the ANSI color code
+            (;[0-9]{1,3})*  # optional ANSI color properties (mainly xterm)
+            )?              # end of color definition
+            m               # the CSI SGR (Select Graphic Rendition) command
+        """, '', s, flags=re.VERBOSE)
     else:
         return s
--- tests/test_strip_color.py
+++ tests/test_strip_color.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 from painter.painter import paint
@@ -10,12 +11,27 @@ def test_strip_color_from_string():
     ) == 'foofoo'
 
 
+def test_strip_color_from_ls_output():
+    fixtures_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), 'fixtures')
+    )
+    with open(os.path.join(fixtures_dir, 'ls_output_colored.txt')) as f:
+        colored_text = f.read()
+    with open(os.path.join(fixtures_dir, 'ls_output.txt')) as f:
+        text = f.read()
+    assert strip_color(colored_text) == text
+
+
 def test_strip_color_other_sequence_from_string():
     assert strip_color('\x1b[0;33;49;3;9;4mbar\x1b[0m') == 'bar'
 
 
 def test_strip_color_non_string():
     assert strip_color(['hello', 'there']) == ['hello', 'there']
+
+
+def test_strip_color_strips_color_from_painter():
+    assert strip_color(paint.underline.red.on_green('foo')) == 'foo'
 
 
 def test_strip_color_cli():
@@ -26,7 +42,3 @@ def test_strip_color_cli():
         shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     ).communicate()
     assert stdout == b'foofoo\n'
-
-
-def test_strip_color_strips_color_from_painter():
-    assert strip_color(paint.underline.red.on_green('foo')) == 'foo'
